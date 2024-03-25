38155cc5dc2433f0810b41fd51331073a76a2cb3
unknown <PeterDiyo>
2024-03-25 10:56:18 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 1,
+    name: "Angel M",
+    age: 19,
+    image:
+      "https://scontent-tir3-2.xx.fbcdn.net/v/t39.30808-6/273543393_308398914590792_4020906153537254220_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=RciQo7RbklgAX91CRMt&_nc_ht=scontent-tir3-2.xx&cb_e2o_trans=q&oh=00_AfAkq5ovUpu780VmwiFXeED7nC1Nx28UmvIMpoxhcgZvpg&oe=6601D4EF",
+  },
+  {
+    id: 2,
+    name: "Diyo Peter",
+    age: 22,
+    image:
+      "https://scontent-tir3-2.xx.fbcdn.net/v/t39.30808-6/428658994_1564939230927396_1185999538030011565_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=OokP-V4NcjIAX_FEBTP&_nc_oc=AdjJS8K2gfr7tpeFQ6oCEdW13Jn58rAGhta_lN4xkGen3vOarnyIwMRYyUZcsu112ow&_nc_ht=scontent-tir3-2.xx&cb_e2o_trans=q&oh=00_AfBHQwgZDB8reFN9MD7acAd7pYw3RGLYBoeT6WQsltBz1A&oe=660217F1",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image: "https://www.course-api.com/images/people/person-5.jpeg",
+  },
+];
